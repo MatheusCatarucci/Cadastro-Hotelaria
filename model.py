a6e1286b1d81270aa340bf197b0c1b5c2b2a60e9
Matheus Catarucci <matheus.catarucci7@gmail.com>
2026-05-19 13:20:36 -0300

    Requirements

--- model.py
+++ model.py
@@ -1,53 +1,11 @@
-from dao import connection
-from datetime import date
-import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = "sqlite:///hotel.db"
 
-# ──────────────────────────────────────────
-# HÓSPEDES
-# ──────────────────────────────────────────
-def consulta_hospede(id):
-    conn = connection()
-    cursor = conn.cursor(dictionary=True)
-    query = """
-            select * from hospedes
-            where id = %s
-            """
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
-    cursor.execute(query, (id,))
-    retorno = cursor.fetchone()
-
-    cursor.close()
-    conn.close()
-
-    return retorno
-
-
-def add_hospede(nome, email, telefone, cpf):
-    conn = connection()
-    cursor = conn.cursor(dictionary=True)
-    query = """
-            insert into hospedes (nome, email, telefone, cpf)
-            values (%s, %s, %s, %s)
-            """
-
-    cursor.execute(query, (nome.capitalize(), email, telefone, cpf))
-    id = cursor.lastrowid
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return id
-
-
-def consulta_hospedes():
-    conn = connection()
-    cursor = conn.cursor(dictionary=True)
-    query = """
-            select * from hospedes
-            """
-
-    cursor.execute(query)
-    retorno = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return retorno
+SessionLocal = sessionmaker(bind=engine)
